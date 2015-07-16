5b17011d71d3e950b8c87e017ea09ae1a94ee805
Brycen Dorsay <dorsay@live.ca>
2015-07-15 22:09:23 -0700

    Multiple images in carousel
    ng-repeat won't work

--- app/components/home/home.controller.js
+++ app/components/home/home.controller.js
@@ -3,6 +3,14 @@
  */
 app.controller('HomeController', function($scope, $location) {
 
+  $scope.slides = [
+    {
+      img: 'img/carousel/kayaksun1.jpg',
+      heading: 'Go Kayaking',
+      caption: 'A wonderful kayaking adventure'
+    }
+  ];
+
   $(document).ready(function(){
     $('.slider').slider({
       full_width: true,
