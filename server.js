e8b83d91fc50cd2f0451f59a48b9963de6d669cf
Brycen <dorsay@live.ca>
2015-07-26 12:33:51 -0700

    Remove public dir from server file
    Just for testing with nginx

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var http = require('http');
 
 var app = express();
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/'));
 
 var port = 80;
 http.createServer(app).listen(port, function() {
