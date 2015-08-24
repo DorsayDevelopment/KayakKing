79612b1b7bd807a60740868a4058f5a2fd0803ca
Brycen <dorsay@live.ca>
2015-08-24 00:08:52 -0700

    Grunt building for staging or production with config files
    Paths for dev not working. Need to configure requirements in index.html

---
+++ config/config.prod.js
@@ -0,0 +1,5 @@
+var config = module.exports = {};
+
+config.port = 80;
+config.indexPath = '/';
+config.depPath = './dependencies';
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-var express = require('./dependencies/express/lib/express');
+var config = require('./config/config');
+var express = require(config.depPath + '/express/lib/express');
 var http = require('http');
-
 var app = express();
 
-app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + config.indexPath));
 
-var port = 9000;
+var port = config.port;
 http.createServer(app).listen(port, function() {
   console.log("server listening on port " + port);
 });
